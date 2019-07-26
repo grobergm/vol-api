5aa3de4ea09da165f77da87ac99d69ce45c01955
Matt Groberg <grobergmg@gmail.com>
2019-07-26 14:09:55 -0700

    add friends, signup for projects, populate friends projects

--- server.js
+++ server.js
@@ -58,8 +58,9 @@ app.post('/api/authenticate', (req,res)=>{
         });
       }
     })
+    .populate('friends')
+    .populate('projects')
   }
-
 });
 
 app.post('/api/register', (req,res)=>{
@@ -89,46 +90,105 @@ app.post('/api/register', (req,res)=>{
   })
 });
 
-app.get('/api/projects/:id',(req,res)=>{
-  Project.find({host:req.params.id},(err,projects)=>{
+// Follow a friend
+app.put('/api/addFriend/:id',middleware.checkToken,(req,res)=>{
+  User.findOne({_id:req.params.id},(err,user)=>{
     if(err){
-      console.log(err)
+      res.json({
+        success: false,
+        message: err,
+      })
+    } else {
+        user.friends.push(req.body.friendId)
+        user.save(err=>{
+          if(err){
+            res.json({
+              success: false,
+              message: err,
+            })
+          } else{
+            res.json({
+            success: true,
+            message: 'Added Friend!',
+          })
+          }
+        })
+    }
+  })
+})
+
+// Load users from set of id's (friends, or volunteers)
+
+
+// Load Projects for Host ID 
+
+// (((I'm trying populate instead)))
+
+// app.get('/api/projects/:id',(req,res)=>{
+//   Project.find({host:req.params.id},(err,projects)=>{
+//     if(err){
+//       res.json({
+//         success: false,
+//         message: err,
+//       })
+//     } else {
+//       res.json({
+//         success: true,
+//         message: 'got the projects',
+//         projects: projects,
+//       })
+//     }
+//   })
+// })
+
+// Add New Project
+app.post('/api/projects/:id',middleware.checkToken,(req,res)=>{
+
+  const {name,tasks} = req.body;
+  const newProject= new Project({name,host:req.params.id,tasks})
+
+  newProject.save(err=>{
+    if(err){
+      res.json({
+        success: false,
+        message: err,
+      })
     } else {
       res.json({
         success: true,
-        message: 'got the projects',
-        projects:projects,
+        message: 'New Project Saved!'
       })
     }
   })
 })
 
+// Sign up for a project (check token is used to add id to project)
 
-app.post('/api/projects/:id',middleware.checkToken,(req,res)=>{
-
-  const {name,goals} = req.body;
-
-  const newProject= new Project({name,host:req.params.id,goals})
-
-  newProject.save(err=>{
+app.put('/api/projects/signup/:id',middleware.checkToken,(req,res)=>{
+  Project.findOne({_id:req.body.projectId},(err,project)=>{
     if(err){
-      console.log(err)
+      res.json({
+        success: false,
+        message: err,
+      })
     } else {
-      console.log(newProject)
+      project.volunteers.push(req.params.id)
+      project.save(err=>{
+        if(err){
+          res.json({
+            success: false,
+            message: err,
+          })
+        } else{
+          res.json({
+          success: true,
+          message: 'Signed up for project',
+        })
+        }
+      })
     }
   })
 })
-
-// app.put('/api/:id',(req,res)=>{
-//   User.findOneAndUpdate(req.params.id,(err,user)=>{
-//     if(err){
-//       console.log(err)
-//     } else {
-
-//       // update user profile
-//     }
-//   })
-// })
 
 
 app.listen(port, () => console.log(`Server is listening on port: ${port}`));
