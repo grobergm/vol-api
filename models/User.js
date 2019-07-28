6d4b20f834c1e0b1b7e59ebb06101de222be4d1d
Matt Groberg <grobergmg@gmail.com>
2019-07-28 10:19:08 -0700

    remove extra field

--- models/User.js
+++ models/User.js
@@ -4,8 +4,6 @@ const UserSchema = new mongoose.Schema({
 	name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   hash: { type: String, required: true },
-  skills:{ type: Array },
-  impacts:{ type: Array },
   friends:[{ type: mongoose.Schema.Types.ObjectId, ref:"User" }],
   projects:[{ type: mongoose.Schema.Types.ObjectId, ref:"Project" }],
 });
